409d1540503e157cd457e8e79f128a92f9e7870e
kutteai <kutteai978@gmail.com>
2025-06-30 19:41:35 +0100

    updated and fixed the user response issue

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,6 +46,9 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
+             'options' => [
+        'ServerSideEncryption' => 'AES256',
+    ],
         ],
     ],
 
